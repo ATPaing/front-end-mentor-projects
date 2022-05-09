49f3839db62006d90430642b14ff90398b86ec13
ATPaing <39166990+ATPaing@users.noreply.github.com>
2022-05-09 14:29:21 +0630

    Update main.js

--- bookmark-landing-page-master/main.js
+++ bookmark-landing-page-master/main.js
@@ -13,49 +13,26 @@ const mobileNav = document.querySelector('.mobile-nav')
 
 const answers = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.`,`Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa,ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies.Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.`,`Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.`,`Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui Aliquam vitae neque eget nisl gravida pellentesque non ut velit.`]
 
+const tabInfoH3Arr = ['Bookmark in one click','Intelligent search','Share your bookmarks']
 
-for(const featureTabLink of featureTabLinks){
-    featureTabLink.addEventListener('click',() => {
+const tabInfoPArr = [` Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.`,`Our powerful search feature will help you find saved sites in no time at all. 
+No need to trawl through all of your bookmarks.`,`Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.`]
+
+
+featureTabLinks.forEach((FTL,i) => {
+    FTL.addEventListener('click' , () => {
+        console.log(FTL)
+        console.log(i)
         removeAllActiveTabNav()
-        if(featureTabLink === featureTabLinks[0]){
 
-            featureTabLink.classList.add('active-tab-nav')
-            tabImg.innerHTML = `
-            <img src="images/illustration-features-tab-1.svg" alt="feature section tab 1 img">
-            `
-            tabInfoH3.textContent = "Bookmark in one click"
-            tabInfoP.textContent = `
-                Organize your bookmarks however you like. Our simple drag-and-drop interface 
-                gives you complete control over how you manage your favourite sites.
-            `
-            
-        }else if(featureTabLink === featureTabLinks[1]){
-
-            featureTabLink.classList.add('active-tab-nav')
-            tabImg.innerHTML = `
-            <img src="images/illustration-features-tab-2.svg" alt="feature section tab 2 img">
-            `
-            tabInfoH3.textContent = "Intelligent search"
-            tabInfoP.textContent = `
-                Our powerful search feature will help you find saved sites in no time at all. 
-                No need to trawl through all of your bookmarks.
-            `
-
-        }else if(featureTabLink === featureTabLinks[2]){
-
-            featureTabLink.classList.add('active-tab-nav')
-            tabImg.innerHTML = `
-            <img src="images/illustration-features-tab-3.svg" alt="feature section tab 3 img">
-            `
-            tabInfoH3.textContent = "Share your bookmarks"
-            tabInfoP.textContent = `
-                Easily share your bookmarks and collections with others. Create a shareable 
-                link that you can send at the click of a button.
-            `
-
-        }
-    }) 
-}
+        FTL.classList.add('active-tab-nav')
+        tabImg.innerHTML = `<img src="images/illustration-features-tab-${i + 1}.svg" alt="feature section tab 1 img">
+        `
+        tabInfoH3.textContent = tabInfoH3Arr[i]
+        tabInfoP.textContent = tabInfoPArr[i]
+     
+    })
+})
 function removeAllActiveTabNav(){
     for(let i = 0; i < featureTabLinks.length ; i++){
         featureTabLinks[i].classList.remove('active-tab-nav')
